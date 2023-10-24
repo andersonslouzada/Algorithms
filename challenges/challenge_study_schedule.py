eb616d3d5d8bca13558d6861d079ef7e7e29b91a
andersonlouzada <anderson.s.louzada@gmail.com>
2023-10-24 11:54:00 -0300

    feat: create function study_shcedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        counter = 0
+        for entrance, exit in permanence_period:
+            if entrance <= target_time <= exit:
+                counter += 1
+        return counter
+    except TypeError:
+        return None

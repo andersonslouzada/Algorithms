7bfe529714cf0fc6c7777c86e33abd2176d9cd00
andersonlouzada <anderson.s.louzada@gmail.com>
2023-10-25 10:11:57 -0300

    test: create test to req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 'abcdef')
+
+    assert encrypt_message('abcdef', 0) == 'fedcba'
+    assert encrypt_message('abcdef', 1) == 'a_fedcb'
+    assert encrypt_message('abcdef', 2) == 'fedc_ba'
+    assert encrypt_message('abcdef', 3) == 'cba_fed'
+    assert encrypt_message('abcdef', 4) == 'fe_dcba'
+    assert encrypt_message('abcdef', 5) == 'edcba_f'

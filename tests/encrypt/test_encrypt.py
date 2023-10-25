38c9f88f6e0c1bf74b81462be15baaa8fd928079
andersonlouzada <anderson.s.louzada@gmail.com>
2023-10-25 10:44:50 -0300

    fix: correct the test from req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,11 +4,13 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     with pytest.raises(TypeError):
-        encrypt_message(1, 'abcdef')
+        encrypt_message(1, 'Anderson')
 
-    assert encrypt_message('abcdef', 0) == 'fedcba'
-    assert encrypt_message('abcdef', 1) == 'a_fedcb'
-    assert encrypt_message('abcdef', 2) == 'fedc_ba'
-    assert encrypt_message('abcdef', 3) == 'cba_fed'
-    assert encrypt_message('abcdef', 4) == 'fe_dcba'
-    assert encrypt_message('abcdef', 5) == 'edcba_f'
+    assert encrypt_message('Anderson', 0) == 'nosrednA'
+    assert encrypt_message('Anderson', 1) == 'A_nosredn'
+    assert encrypt_message('Anderson', 2) == 'nosred_nA'
+    assert encrypt_message('Anderson', 3) == 'dnA_nosre'
+    assert encrypt_message('Anderson', 4) == 'nosr_ednA'
+    assert encrypt_message('Anderson', 5) == 'rednA_nos'
+    assert encrypt_message('Anderson', 6) == 'no_srednA'
+    assert encrypt_message('Anderson', 7) == 'osrednA_n'
